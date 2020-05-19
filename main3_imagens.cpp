b82c716f60341995eb6487685cb2789fe411b959
NascimentoG <gus_nascimento98@hotmail.com>
2020-05-19 17:05:50 -0300

    Dataset

---
+++ main3_imagens.cpp
@@ -0,0 +1,36 @@
+/*Autores:
+    Anniely Soares Lemos 
+    Gustavo Henrique do Nascimento
+*/
+
+#include "linhas.h"
+
+int main(int argc, char *argv[]){
+    string pref = "_metedo4_sainda.png";
+    string path = "imagens/";
+    string name;
+    Camera cam(0,4);
+    cam.img_fn += argv[1];
+    cam.img_fn += cam.img_ext;
+    cam.frame = imread(cam.img_fn, IMREAD_COLOR);
+    cam.gettingSize(cam.frame.cols,cam.frame.rows);
+   
+    cam.frame.copyTo(cam.frame_final);
+   
+    cam.creatingRoi(cam.frame);
+    cam.Segmentation(cam.frame_roi);
+    cam.morphologicalOperations(cam.segmented);
+    cam.SegAndCluster(cam.skeleton,30);
+    cam.estimated_lines();
+    cam.drawLines();
+
+    name += path;
+    name += argv[1];
+    name += pref;
+    cam.writingFile(argv[1]);
+    imwrite(name,cam.frame_final);
+    cam.cap.release();
+    destroyAllWindows();
+
+	return 0;
+}
